d9baf77999e788c47608ad70a183d1a79be1066c
Sebastian Mainero <sebastianmperea@gmail.com>
2021-05-15 16:18:10 -0500

    finished left side

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,59 @@
 <script>
   import LogoIcon from "./components/LogoIcon.svelte";
+  import Card from "./components/Card.svelte"
+  let formData = { name: "", email: "", link: "" };
+
+  const handleForm = async (e) => {
+    console.log(formData);
+    formData = { name: "", email: "", link: "" };
+    e.preventDefault();
+  };
 </script>
 
 <main>
   <div class={"container"}>
     <div class="topBar"><LogoIcon height={40} width={300} /></div>
     <div class={"blocksContainer"}>
-      <div class={"leftBlock"}>
-        <h1>enter the data to the register</h1>
-        <form class="content">
-          <label>
-			<input  placeholder={"User Name"} type="text"  />
 
-			</label>
+      <div class={"leftBlock"}>
+        <h1 class={"leftBlockTitle"}>enter the data <br />to the register</h1>
+
+        <form on:submit={handleForm} class="content">
+          <div class={"formContainer"}>
           <label>
-			<input placeholder={"Email"} type="text"  />
-			</label>
+            <input
+              bind:value={formData.name}
+              placeholder={"User Name"}
+              type="text"
+              required
+            />
+          </label>
           <label>
-			<input placeholder={"Image Link"} type="text"  />
-			</label>
-			<button>SEND</button>
+            <input
+              bind:value={formData.email}
+              placeholder={"Email"}
+              type="text"
+              required
+            />
+          </label>
+          <label>
+            <input
+              bind:value={formData.link}
+              placeholder={"Image Link"}
+              type="text"
+              required
+            />
+          </label>
+          <button class={"formButton"} type="submit">SEND</button>
+        </div>
         </form>
       </div>
-      <div class={"rightBlock"} />
+      <div class={"rightBlock"} >
+      <h1 class={"rightBlockTitle"}>
+      UPDATE <br>RECORDED DATA
+      </h1>
+      <Card/>
+      </div>
     </div>
   </div>
 </main>
@@ -34,9 +65,6 @@
     padding: 0;
     box-sizing: border-box;
   }
-input{
-	background-color: black;
-}
   .container {
     display: flex;
     flex-direction: column;
@@ -45,7 +73,7 @@ input{
   }
   .topBar {
     width: 100%;
-    height: 86px;
+    height: 100px;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -58,14 +86,53 @@ input{
     width: 100%;
     height: 100%;
   }
+
   .leftBlock {
     display: flex;
-	flex-direction: column;
-    justify-content: center;
+    flex-direction: column;
+    /* justify-content: center; */
     align-items: center;
     background-color: #000000;
     width: 50%;
+  }  
 
+  .leftBlockTitle {
+    margin-bottom: 50px;
+    word-spacing: 5px;
+    letter-spacing: 5px;
+    margin-top:80px;
+    color: #ffffff;
+    text-transform: uppercase;
+    font-family: "Montserrat", sans-serif;
+    font-size: 3em;
+  }
+  .formContainer{
+    display:flex;
+    flex-direction: column;
+    /* justify-content: space-around; */
+    align-items: center;
+    height:320px;
+  }
+  input {
+    background-color: black;
+    color: white;
+    width:400px;
+    height: 55px;
+    margin-top: 20px;
+  }
+  ::placeholder{
+    color:white;
+    opacity: 80%;
+    padding-left:10px;
+  }
+  .formButton{
+    color:black;
+    font-weight: bold;
+    background-color:white;
+    margin-top: 50px;
+    cursor: pointer;
+    padding: 0.8rem 4.5rem;
+    font-size: 1.6rem;
   }
   .rightBlock {
     background-color: #ffffff;
@@ -84,18 +151,21 @@ input{
     border-right: solid 40px white;
     border-bottom: solid 23px transparent;
   }
-
+  .rightBlockTitle {
+    margin-bottom: 50px;
+    word-spacing: 5px;
+    letter-spacing: 5px;
+    margin-top:80px;
+    color: #000000;
+    text-transform: uppercase;
+    font-family: "Montserrat", sans-serif;
+    font-size: 3em;
+  }
   main {
     text-align: center;
     margin: 0 auto;
   }
 
-  h1 {
-    color: #ffffff;
-    text-transform: uppercase;
-    font-family: "Montserrat", sans-serif;
-    font-size: 3em;
-  }
 
   @media (min-width: 640px) {
     main {
---
+++ src/components/Card.svelte
@@ -0,0 +1,40 @@
+<script>
+  let Image = "assets/image.png";
+  export let link = Image;
+  export let name = "Juliet martín";
+  export let email = "julietmartin1221@gmail.com";
+</script>
+
+<div class={"card"}>
+  <img src={link} alt={"Image Link"} />
+  <div class={"cardText"}>
+    <h1 class={"name"}>{name}</h1>
+    <p class={"email"}>{email}</p>
+  </div>
+</div>
+
+<style>
+  .card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 400px;
+    height: 250px;
+    background-color: black;
+  }
+  .cardText {
+    display: flex;
+    flex-direction: column;
+    justify-items: space-between;
+    align-content: center;
+  }
+  .name {
+    text-transform: capitalize;
+    font-family: "Montserrat", sans-serif;
+  }
+  .email {
+    text-transform: lowercase;
+  }
+
+</style>
